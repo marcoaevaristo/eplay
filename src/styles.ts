75c2e32b1c4bc35d6b696b6424aac54fba23f367
Marco Antonio Evaristo <120408442+marcoaevaristo@users.noreply.github.com>
2024-06-23 04:02:46 -0300

    Criação do Header

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const cores = {
+export const cores = {
   branca: '#eeeeee',
   preta: '#111',
   cinza: '#333',
@@ -8,14 +8,22 @@ const cores = {
 }
 
 export const GlobalCss = createGlobalStyle`
-{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: Roboto, sans-serif;
+  list-style: none;
 }
   body {
   background-color: ${cores.preta};
   color: ${cores.branca};
+  padding-top: 40px;
   }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 `
